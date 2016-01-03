0185f53c63a16c749278d69624695bffcbe9c701
Justin <me@justinmaslin.com>
2016-01-03 16:25:09 -0500

    Init

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name birthdayApp
+ * @description
+ * # birthdayApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('birthdayApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name birthdayApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the birthdayApp
+ */
+angular.module('birthdayApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
