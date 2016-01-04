0da65bcb57e2084686ad03a333ad72d65183bc9c
Justin <me@justinmaslin.com>
2016-01-04 18:50:49 -0500

    Add basic app logic

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,26 @@
  * Controller of the birthdayApp
  */
 angular.module('birthdayApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $log, $timeout, birthdayFetcher) {
+
+    $scope.date = new Date();
+    var person;
+
+    $scope.changeDate = function changeDate (date) {
+      birthdayFetcher.getPeopleForDay(date).then(function () {
+        $scope.getPerson();
+      });
+    };
+
+    $scope.getPerson = function getPerson () {
+      delete $scope.person;
+      $timeout(function () {
+        $log.debug("Get new person.");
+        $scope.person = birthdayFetcher.getRandomPerson();
+        $log.info($scope.person);
+      }, 50);
+    };
+
+    $scope.changeDate($scope.date);
+
   });
